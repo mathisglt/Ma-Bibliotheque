cc199266a0919cab7af658cc91b05d0741308c9b
Mathis <mathis.gaultier@etudiant.univ-rennes1.fr>
2024-11-28 09:59:29 +0100

    Change colors + about me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,9 +4,18 @@ import './About.css';
 const About: React.FC = () => {
   return (
     <div className="about-container">
-      <h2>Moi</h2>
-      <p>Projet créé par <span>Gaultier Mathis</span></p>
-      <p>Module WE</p>
+      <h2>À propos de Moi</h2>
+      <p>
+        Bonjour, je suis <span>Gaultier Mathis</span>,<br></br> étudiant en Master 1 Ingénierie Logicielle (M1 IL).
+      </p>
+      <p>Bienvenue sur mon projet !</p>
+      <button
+        className="cta-button"
+        onClick={() => window.open('https://github.com/mathisglt', '_blank')}
+      >
+        Mon GitHub
+      </button>
+
     </div>
   );
 };
